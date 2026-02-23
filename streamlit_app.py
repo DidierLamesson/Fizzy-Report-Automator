a48d0fd3a5426b9a559c2ea09266bb29df55c4d9
DidierLamesson <50462604+DidierLamesson@users.noreply.github.com>
2026-02-23 22:05:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -274,6 +274,7 @@ if uploaded and restaurant_input:
 
         # Axe Y
         ax.tick_params(axis='y', colors=COLORS["white"], labelsize=14)
+        ax.set_yticks([])
         ax.yaxis.set_major_formatter(
             ticker.FuncFormatter(
                 lambda x, p: f"{int(x):,}".replace(",", " ")
